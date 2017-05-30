6888092623495eb3ac2e92e53d62bde9b95f6697
simboyy <smkorera@gmail.com>
2017-05-30 16:25:51 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -63,7 +63,7 @@ var socketio = require('socket.io')(server, {
 require('./config/socketio').default(socketio);
 require('./config/express').default(app);
 require('./routes').default(app);
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/bower_components', _express2.static(__dirname + '/bower_components'));
 
 // Start server
 function startServer() {
